25608a7fef61e5345286ffe53fdff9e300a4294c
DylxnWRLD <dylaniscute.23@gmail.com>
2025-12-05 14:15:28 -0600

    Intento de despliegue 3

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-
-const base = process.env.NODE_ENV === 'production' ? './' : '/'; 
-
 export default defineConfig({
   plugins: [svelte()],
-  base: base,
+  base: './',
   build: {
-    outDir: 'dist', 
+    outDir: 'dist',
   },
 });
