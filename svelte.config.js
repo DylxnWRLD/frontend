2e22fef7a761ebb28b8975e78f9825eb9302be5c
DylxnWRLD <dylaniscute.23@gmail.com>
2025-12-05 14:24:14 -0600

    Intento 4 de despliegue del front

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default defineConfig({
-  plugins: [svelte()],
-  base: './',
-  build: {
-    outDir: 'dist',
-  },
-});
+export default {
+  preprocess: vitePreprocess(),
+  compilerOptions: {
+    dev: true
+  }
+};
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
+  base: '/frontend/',
   server: {
     host: '0.0.0.0',
     watch: {
